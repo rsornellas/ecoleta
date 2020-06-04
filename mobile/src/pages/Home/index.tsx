bd53b9ff95307edb4a905f87e34fa1c7e36dc496
Raphael Ornellas <raphaelso@live.com>
2020-06-04 20:08:36 -0300

    init mobile

---
+++ mobile/src/pages/Home/index.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { View, ImageBackground, Image, Text } from 'react-native';
+
+import { RectButton } from 'react-native-gesture-handler';
+import { Feather } from '@expo/vector-icons';
+
+import { useNavigation } from '@react-navigation/native';
+
+import styles from './styles';
+
+const Home = () => {
+  const navigation = useNavigation();
+
+  function handleNavigateToPoints() {
+    navigation.navigate('Points');
+  }
+
+  return (
+    <ImageBackground
+      style={styles.container}
+      source={require('../../assets/home-background.png')}
+      imageStyle={{ width: 274, height: 368 }}
+    >
+      <View style={styles.main}>
+        <Image source={require('../../assets/logo.png')} />
+
+        <Text style={styles.title}>Seu marketplace de coleta de resíduos</Text>
+        <Text style={styles.description}>
+          Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.
+        </Text>
+      </View>
+
+      <View style={styles.footer}>
+        <RectButton style={styles.button} onPress={handleNavigateToPoints}>
+          <View style={styles.buttonIcon}>
+            <Feather name="arrow-right" color="#fff" size={24} />
+          </View>
+          <Text style={styles.buttonText}>Entrar</Text>
+        </RectButton>
+      </View>
+    </ImageBackground>
+  );
+};
+
+export default Home;
